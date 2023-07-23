218493e32d1d43befd7553ff70e256eba3c38d59
scorelowless <makochonmarek@gmail.com>
2023-07-23 16:50:43 +0200

    Adding high battery level alert

--- low_battery.sh
+++ low_battery.sh
@@ -5,9 +5,10 @@
 #Run 'crontab -e' and write '*/10 * * * * /path/to/this/script' to make it run every 10 minutes
 #Of course you can change the number if you want
 
-#How low does the battery level have to be to turn the notification
+#How low/high does the battery level have to be to send the notification
 #change it if you need
-battery_level_notification_treshold=15
+battery_level_notification_lower_treshold=15
+battery_level_notification_upper_treshold=85
 
 #Calculate the average battery level
 battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)' | awk '{ SUM += $1} END {print SUM}'`
@@ -16,6 +17,13 @@ number_of_batteries=`acpi -b | grep -P -o '[0-9]+(?=%)' | wc -l`
 average_battery_level=$((battery_level / number_of_batteries))
 
 #Send notification if battery level is below the set treshold
-if [ $average_battery_level -le $battery_level_notification_treshold] then
+if [ $average_battery_level -le $battery_level_notification_lower_treshold ]
+then
     notify-send "Low battery level!" "${average_battery_level}%"
+fi
+
+#Send notification if battery level is above the set treshold
+if [ $average_battery_level -ge $battery_level_notification_upper_treshold ]
+then
+    notify-send "High battery level: ${average_battery_level}%" "Remove the charger!"
 fi
